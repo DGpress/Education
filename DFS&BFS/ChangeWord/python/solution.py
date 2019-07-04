e48ab3603365a33f0faa0d409b79fb6c127b0ae9
DGpress <bgbg9655@naver.com>
2019-07-04 16:11:05 +0900

    [MOD] solution.py
    modify solution.py with code running exaples.

--- DFS&BFS/ChangeWord/python/solution.py
+++ DFS&BFS/ChangeWord/python/solution.py
@@ -1,3 +1,7 @@
 def solution(begin, target, words):
     answer = 0
     return answer
+
+# Run examples
+print(solution("hit", "cog", ["hot","dot","dog","lot","log","cog"])) # return 4
+print(solution("hit", "cog", ["hot","dot","dog","lot","log"])) # return 0
