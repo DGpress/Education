b44e122fa31f504e64d2509846b85effdefc645a
DGpress <bgbg9655@naver.com>
2019-07-04 16:32:34 +0900

    [MOD] solution.py
    modify solution.py with annotations.

--- DFS&BFS/ChangeWord/python/solution.py
+++ DFS&BFS/ChangeWord/python/solution.py
@@ -1,4 +1,21 @@
+'''
+    DFS&BFS ChangeWord
+    ChangeWord_README.md is explained this simulation.
+'''
+
 def solution(begin, target, words):
+    '''
+        Main body
+
+        begin : (str) the first word for changing to target word.
+        target : (str) the target word.
+        words : (list) it has variety words.
+
+        This simulation returns the least number of changing.
+        Along simulation's conditions, word is changed another word one by one spell.
+        If changed word is a target word, this simulation is ended.
+        Before ending, this code block is returned the number of changes.
+    '''
     answer = 0
     return answer
 
